d28206342f3c15c3214c2b72771a749ee8726d33
imizao <2313466338@qq.com>
2019-12-12 17:45:07 +0800

    初始化

---
+++ src/page/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+function App() {
+  return (
+    <div className="App">
+      <Button>
+          123
+        </Button>
+    </div>
+  );
+}
+
+export default App;
